64afd818c6fdd9916ccde5cce4230f9cde449bfc
John Long <johnscrazy@googlemail.com>
2019-11-28 00:40:04 +0100

    Change README and apply some recommended styles

--- README.md
+++ README.md
@@ -1,24 +1,68 @@
 # vue-roughviz
 
-## Project setup
+It's a Vue-wrapper for [roughViz.js](https://github.com/jwilber/roughViz). See examples in the `App.vue`.
+
+## Installation
 ```
-npm install
+npm install vue-roughviz
 ```
 
-### Compiles and hot-reloads for development
+## Usage
 ```
+<h3>Donut</h3>
+<rough-donut
+    :data="{
+    labels: ['North', 'South', 'East', 'West'],
+    values: [10, 5, 8, 3]
+    }"
+    title="Regions"
+    roughness="8"
+    :colors="['red', 'orange', 'blue', 'skyblue']"
+    stroke="black"
+    stroke-width="3"
+    fill-style="cross-hatch"
+    fill-weight="3.5"
+/>
+```
+
+## Development
+```
+npm i
 npm run serve
 ```
-
-### Compiles and minifies for production
-```
-npm run build
-```
+In case it is not working,
+this repo uses the [Vue-Cli](https://cli.vuejs.org).
 
 ### Lints and fixes files
 ```
 npm run lint
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+## Declaration and Style
+
+The components are following the [Vue Style Guide](https://vuejs.org/v2/style-guide/).
+That means components are called in [Kebab-Style](https://vuejs.org/v2/style-guide/#Self-closing-components-strongly-recommended):
+```
+<rough-donut></rough-donut>
+```
+
+For each attribute, a line in a component and also in Kebab-Style is [recommended](https://vuejs.org/v2/style-guide/#Multi-attribute-elements-strongly-recommended). However,
+both is possible.
+
+```
+fill-weight="0.35" // better than fillWeight
+stroke-width="0.5" // better than strokeWidth
+fill-style="cross-hatch" // better than fillStyle
+```
+
+By passing an object, Vue needs to [bind](https://vuejs.org/v2/guide/class-and-style.html) it.
+
+```
+<rough-pie>
+:data="[
+    { month: 'Jan', A: 20, B: 5, C: 10 },
+    { month: 'Feb', A: 25, B: 10, C: 20 },
+    { month: 'March', A: 30, B: 50, C: 10 }
+]"
+</rough-pie>
+```
--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
           data="https://raw.githubusercontent.com/jwilber/random_data/master/flavors.csv"
           labels="flavor"
           values="price"
-        />
+        ></rough-bar>
       </div>
       <div class="flex-1 text-center px-4 py-2 m-2">
         <h3>Donut</h3>
@@ -24,7 +24,7 @@
           stroke-width="3"
           fill-style="cross-hatch"
           fill-weight="3.5"
-        />
+        ></rough-donut>
       </div>
       <div class="flex-1 text-center px-4 py-2 m-2">
         <h3>Pie</h3>
@@ -36,7 +36,7 @@
           title="Regions"
           :colors="['red', 'orange', 'blue', 'skyblue']"
           roughness="8"
-        />
+        ></rough-pie>
       </div>
     </div>
     <div class="flex">
@@ -47,7 +47,7 @@
           y1="revenue"
           y2="cost"
           y3="profit"
-        />
+        ></rough-line>
       </div>
       <div class="flex-1 text-center px-4 py-2 m-2">
         <h3>Scatter</h3>
@@ -70,7 +70,7 @@
           x-label="sepal width"
           y-label="petal length"
           curb-zero="false"
-        />
+        ></rough-scatter>
       </div>
       <div class="flex-1 text-center px-4 py-2 m-2">
         <h3>Stacked Bar</h3>
@@ -88,7 +88,7 @@
           stroke-width="0.5"
           fill-style="cross-hatch"
           stroke="black"
-        />
+        ></rough-stacked-bar>
       </div>
     </div>
     <div class="flex">
@@ -98,16 +98,21 @@
           title="Vehicles I've Had"
           title-font-size="1.5rem"
           legend="false"
-          :margin="{top: 50, bottom: 100, left: 160, right: 0}"
+          :margin="{ top: 50, bottom: 100, left: 160, right: 0 }"
           :data="{
-            labels: ['1992 Ford Aerostar Van', '2013 Kia Rio', '1980 Honda CB 125s', '1992 Toyota Tercel'],
+            labels: [
+              '1992 Ford Aerostar Van',
+              '2013 Kia Rio',
+              '1980 Honda CB 125s',
+              '1992 Toyota Tercel'
+            ],
             values: [8, 4, 6, 2]
           }"
           x-label="Time Owned (Years)"
           stroke-width="2"
           fill-style="zigzag-line"
           highlight="gold"
-        />
+        ></rough-bar-h>
       </div>
     </div>
   </div>
